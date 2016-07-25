c4f15c2f14e94aaf365deee8d986b3ee64fe6c88
gavinzhou <gavin.zhou@gmail.com>
2016-07-25 08:38:00 +0000

    first commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+_kong_version=$1
+_kong_tag=$2
+
+if [ -z "${_kong_version}" ]; then
+    source _VERSION
+    _kong_version=$_VERSION
+    _kong_tag=$_VERSION
+
+fi
+
+docker build --build-arg KONG_VERSION=${_kong_version} --tag "orangesys/alpine-kong:${_kong_tag}" --no-cache=true .
