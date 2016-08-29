f45b399a8631885a4cf6b0464261bc9da1a426c4
gavinzhou <gavin.zhou@gmail.com>
2016-08-29 09:28:29 +0000

    gitignore checkkong.sh

--- 0.9.0/chechkong.sh
+++ 0.9.0/chechkong.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-curl -sX POST \
- --url http://127.0.0.1:8001/apis/ \
- --data 'name=httpbin' \
- --data 'upstream_url=http://httpbin.org/' \
- --data 'request_host=kong.httpbin.org'
 
-curl -sX GET \
-  --url http://127.0.0.1:8001/apis/ \
-  |jq '.data[].name == "httpbin"'
+http POST http://127.0.0.1:8001/apis/ name=httpbin upstream_url=http://httpbin.org/ request_host=kong.httpbin.org
 
-curl -sX GET \
-  --header "Host: kong.httpbin.org" \
-  --url http://127.0.0.1:8000/get \
-  |jq '.headers["Accept"] == "*/*"'
+http http://127.0.0.1:8001/apis/httpbin
 
-curl -sX DELETE \
---url http://127.0.0.1:8001/apis/httpbin
+http http://127.0.0.1:8000/ip Host:kong.httpbin.org
 
-curl -sX GET \
- --url http://127.0.0.1:8001/apis/ \
- |jq '.data[].name != "httpbin"'
+#curl -sX GET \
+#  --url http://127.0.0.1:8001/apis/ \
+#  |jq '.data[].name == "httpbin"'
+#
+#curl -sX GET \
+#  --header "Host: kong.httpbin.org" \
+#  --url http://127.0.0.1:8000/get \
+#  |jq '.headers["Accept"] == "*/*"'
+#
+#curl -sX DELETE \
+#--url http://127.0.0.1:8001/apis/httpbin
+#
+#curl -sX GET \
+# --url http://127.0.0.1:8001/apis/ \
+# |jq '.data[].name != "httpbin"'
