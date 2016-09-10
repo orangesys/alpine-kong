55e86b13c6e8c54bf7b8c9df9af37e5e16f027c5
Gavin Zhou <gavin.zhou@gmail.com>
2016-09-10 17:51:21 +0900

    fix debug mode with deploy.sh echo something

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,6 @@ for path in $dockerfiles; do
   # Generate a tag by replacing the first slash with a colon and all remaining slashes with a dash.
   tag=$(echo $path | sed 's@/@:@' | sed 's@/@-@g')
   log_msg "Tagging docker image $tag with gcr tag"
-  echo "docker tag "orangesys/alpine-kong:${tag}" "asia.gcr.io/orange-sys/alpine-kong:${tag}""
-  echo "sudo /opt/google-cloud-sdk/bin/gcloud docker push asia.gcr.io/orange-sys/alpine-kong:${tag}"
-
+  docker tag "orangesys/alpine-kong:${tag}" "asia.gcr.io/orange-sys/alpine-kong:${tag}"
+  sudo /opt/google-cloud-sdk/bin/gcloud docker push asia.gcr.io/orange-sys/alpine-kong:${tag}
 done
