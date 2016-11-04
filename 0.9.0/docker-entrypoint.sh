15200064f0ea5f02e82af1f240dac5d9d0d74832
gavinzhou <gavin.zhou@gmail.com>
2016-11-04 14:24:57 +0000

    add docker-compose test

--- 0.9.0/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/dumb-init /bin/bash
-set -e
-
-# Disabling nginx daemon mode
-export KONG_NGINX_DAEMON="off"
-
-[ -z "$KONG_NGINX_DAEMON" ] && export KONG_NGINX_DAEMON="off"
-
-exec "$@"
---
+++ check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+curl -si -X POST \
+  --url http://localhost:8001/apis/ \
+  --data 'name=mockbin' \
+  --data 'upstream_url=http://mockbin.com/' \
+  --data 'request_host=mockbin.com'
+
+curl -si -X POST \
+  --url http://localhost:8001/apis/mockbin/plugins/ \
+  --data 'name=jwt'
+
+curl -si -X POST \
+  --url http://localhost:8001/consumers/ \
+  --data "username=mockbin"
+
+
+curl -si -X POST \
+   --url http://localhost:8001/consumers/mockbin/jwt \
+   --data 'key=a36c3049b36249a3c9f8891cb127243c' \
+   --data 'secret=e71829c351aa4242c2719cbfbe671c09'
+
+curl --retry 10 --retry-delay 5 -s http://localhost:8000/request?\
+jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.\
+eyJpc3MiOiJhMzZjMzA0OWIzNjI0OWEzYzlmODg5MWNiMTI3MjQzYyIsImV4cCI6MTQ0MjQzMDA1NCwibmJmIjoxNDQyNDI2NDU0LCJpYXQiOjE0NDI0MjY0NTR9.\
+AhumfY35GFLuEEjrOXiaADo7Ae6gt_8VLwX7qffhQN4 \
+--header "Host: mockbin.com"|grep -q GET
