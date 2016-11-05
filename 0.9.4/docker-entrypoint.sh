3af2356c363ca9cf2fd4a50b7bfeaf2c2108858f
Gavin Zhou <gavin.zhou@gmail.com>
2016-11-06 01:04:36 +0900

    link loggint to stdout

--- 0.9.4/docker-entrypoint.sh
+++ 0.9.4/docker-entrypoint.sh
@@ -4,6 +4,11 @@ set -e
 # Disabling nginx daemon mode
 export KONG_NGINX_DAEMON="off"
 
+# forward request and error logs to docker log collector
+mkdir -p /var/log/nginx
+ln -sf /dev/stdout /var/log/nginx/access.log
+ln -sf /dev/stderr /var/log/nginx/error.log
+
 [ -z "$KONG_NGINX_DAEMON" ] && export KONG_NGINX_DAEMON="off"
 
 exec "$@"
