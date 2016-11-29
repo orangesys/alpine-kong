764b3093f5dfb85110c54765892a5acd632fbb60
Gavin Zhou <gavin.zhou@gmail.com>
2016-11-29 18:32:13 +0900

    fix push to gcr

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ for path in $dockerfiles; do
   tag=$(echo $path | sed 's@/@:@' | sed 's@/@-@g')
   log_msg "Tagging docker image $tag with gcr tag"
   docker tag "orangesys/alpine-kong:${tag}" "asia.gcr.io/saas-orangesys-io/alpine-kong:${tag}"
-  sudo /opt/google-cloud-sdk/bin/gcloud docker push asia.gcr.io/saas-orangesys-io/alpine-kong:${tag}
+  sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/saas-orangesys-io/alpine-kong:${tag}
 done
